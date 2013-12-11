6e1a4cd1b4af0580cc9eee8f47806595fed7783e
Jukka Suomela <jukka.suomela@iki.fi>
2013-12-11 14:46:13 +0200

    How to compile.

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$CXX" = "" ]; then
+    CXX="g++"
+fi
+
+if [ "$CXXFLAGS" = "" ]; then
+    CXXFLAGS="-g -std=c++11 -march=native -O3 -Werror -Wall -Wextra -Wunused -Wold-style-cast -Woverloaded-virtual -Wsign-promo -Wdouble-promotion -Wconversion -Wsign-conversion -Wmissing-declarations -Wredundant-decls -Wno-unknown-pragmas"
+fi
+
+if [ "$OMPFLAGS" = "" ]; then
+    OMPFLAGS="-fopenmp"
+fi
+
+mkdir -p bin || exit 1
+
+$CXX $CXXFLAGS           -DTEST_SIMPLE -o bin/test-simple test.cc || exit 1
+$CXX $CXXFLAGS $OMPFLAGS -DTEST_OPENMP -o bin/test-openmp test.cc || exit 1
