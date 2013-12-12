be9e2bdc3cfb021df7a676323f72beda036d85a6
Jukka Suomela <jukka.suomela@iki.fi>
2013-12-12 16:33:25 +0200

    Separate binary files for icc.

--- build-icc.sh
+++ build-icc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CXX=icpc CXXFLAGS="-Wall -g -O3 -msse4.2 -restrict -std=c++11" ./build.sh || exit 1
+BINSUFFIX=-icc CXX=icpc CXXFLAGS="-Wall -g -O3 -msse4.2 -restrict -std=c++11" ./build.sh || exit 1
